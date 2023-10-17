f3ba197bab14be18681b337a5c7c87f65d69fee9
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-10-17 15:25:48 +0900

    set up sample code

---
+++ src/components/SearchComponent.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const SearchComponent = ({ searchCourse, courseSearchUserFunction }) => {
+	return (
+		<header className="App-header">
+			<h1>GeeksforGeeks Shopping Cart</h1>
+			<div className="search-bar">
+				<input
+					type="text"
+					placeholder="Search for GFG Products..."
+					value={searchCourse}
+					onChange={courseSearchUserFunction}
+				/>
+			</div>
+		</header>
+	);
+}
+
+export default SearchComponent;
